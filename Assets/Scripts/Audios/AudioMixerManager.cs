f46c89a68c1a2ff9df1269cee5b49ffd24697d3d
KaprisDimitri <kaprisdimitri@gmail.com>
2022-07-04 17:24:20 +0200

    EventManager

--- Assets/Scripts/Audios/AudioMixerManager.cs
+++ Assets/Scripts/Audios/AudioMixerManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.Audio;
 public class AudioMixerManager : MonoBehaviour
 {
     [SerializeField] AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        InitAudioMixerManager();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,10 @@ public class AudioMixerManager : MonoBehaviour
     void Update()
     {
         
+    }
+    void InitAudioMixerManager ()
+    {
+        EventManager.ChangeVolumeMixer += ChangeVolume;
     }
 
     public void ChangeVolume (ChoseCanal canal, float value)
