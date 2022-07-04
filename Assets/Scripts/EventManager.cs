1e1f1b45bc0fa306c0da1d354ef1b1d079a3621a
KaprisDimitri <kaprisdimitri@gmail.com>
2022-07-04 17:32:11 +0200

    Event manager pas monoBehavior

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -3,19 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class EventManager : MonoBehaviour
+public class EventManager
 {
     public static event Action<AdioManager.TypeOfSound,int,Vector3,float> PlaySound;
     public static event Action<AudioMixerManager.ChoseCanal, float> ChangeVolumeMixer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
