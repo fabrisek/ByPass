e66275f3bf4d7a7db8bbbde55f57c8045a9b2e75
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-07-06 17:48:07 +0200

    player

--- Assets/FallingEffect.cs
+++ Assets/FallingEffect.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class FallingEffect : MonoBehaviour
 {
     // Start is called before the first frame update
-    float startY;
-    [SerializeField] float range;
+    [SerializeField] float speed;
     [SerializeField] Rigidbody rb;
     [SerializeField] Transform upPosition;
     [SerializeField] Transform downPosition;
+
     void Start()
     {
-        startY = transform.position.y;
+
     }
 
     // Update is called once per frame
@@ -21,17 +21,17 @@ public class FallingEffect : MonoBehaviour
         //go up
         if (rb.velocity.y > 0.5f)
         {
-            transform.position = Vector3.Lerp(transform.position, upPosition.position, Time.deltaTime*2);
+            transform.position = Vector3.Lerp(transform.position, upPosition.position, Time.deltaTime*speed);
         }
         //goDown
         else if(rb.velocity.y < -0.5f)
         {
-            transform.position = Vector3.Lerp(transform.position,downPosition.position, Time.deltaTime*2);
+            transform.position = Vector3.Lerp(transform.position,downPosition.position, Time.deltaTime*speed);
         }
         //wigglegun
         else
         {
-            transform.position = Vector3.Lerp(transform.position, downPosition.position, Time.deltaTime);
+
         }
     }
 }
